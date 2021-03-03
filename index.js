78ff99f1c779df7bddc8cc83e2573eaaaf19b973
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-03-03 14:53:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { token, prefix } = require("./config.json");
+const { token, prefix, guildSupport } = require("./config.json");
 
 const Discord = require("discord.js");
 const fs = require("fs");
@@ -18,20 +18,96 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
-client.on("message", message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (!client.commands.has(command)) {
-        message.reply(`Je ne possède pas cette commande: ${command}`);
+client.on("message", async message => {
+    if (message.author.bot) {
+        return;
     }
-    try {
-        client.commands.get(command).execute(message, args, client);
+
+    if (message.channel.type === "dm") {
+        const msg = message.content;
+
+        const guild = client.guilds.cache.find(g => g.id === `${guildSupport}`);
+
+        let categorie = guild.channels.cache.find(c => c.name === "Tickets" && c.type === "category");
+        if (!categorie) categorie = await guild.channels.create("Tickets", {type: "category", position: 1});
+
+        if (!guild.channels.cache.find(c => c.name === `${message.author.id}-mp`)) {
+            guild.channels.create(`${message.author.id}-mp`, {
+                permissionOverwrites: [
+                    {
+                        deny: "VIEW_CHANNEL",
+                        id: guild.id
+                    },
+                ],
+                parent: categorie.id,
+                topic: `${message.author.id}`,
+            })
+            .then(ch => {
+                const e = new Discord.MessageEmbed()
+                .setTitle("Un membre demande de l'aide")
+                .setColor("#2F3136")
+                .setDescription(`Utilisateur: ${message.author.tag}\nID: ${message.author.id}`)
+                .setFooter("Merci de cliquer sur 🔒 pour fermer le ticket")
+                .addField("Sa question", msg)
+
+                ch.send(e)
+                .then(msg => {
+                    msg.react("🔒")
+                })
+            })
+        }
+        else {
+            const channelTicket = guild.channels.cache.find(c => c.name === `${message.author.id}-mp`)
+
+            const e = new Discord.MessageEmbed()
+            .setTitle("Une nouvelle question")
+            .setColor("#2F3136")
+            .addField("Sa question", msg)
+
+            channelTicket.send(e)
+        }
     }
-    catch (error) {
-        console.log(error);
+    else {
+        if (message.channel.name.endsWith("-mp")) {
+            const msg = message.content;
+
+            message.delete();
+
+            const e1 = new Discord.MessageEmbed()
+            .setTitle(message.author.tag)
+            .setColor("#2F3136")
+            .setDescription(msg)
+
+            message.channel.send(e1)
+
+            const user = await client.users.fetch(`${message.channel.topic}`);
+
+            const e2 = new Discord.MessageEmbed()
+            .setTitle("Réponse du staff")
+            .setColor("#2F3136")
+            .addField(message.author.tag, msg)
+
+            await user.send(e2)
+            .then(msg => {
+                msg.react("📥")
+            })
+        }
+        else {
+            if (!message.content.startsWith(prefix)) return;
+
+            const args = message.content.slice(prefix.length).split(/ +/);
+            const command = args.shift().toLowerCase();
+
+            if (!client.commands.has(command)) {
+             message.reply(`Je ne possède pas cette commande: ${command}`);
+            }
+            try {
+                lient.commands.get(command).execute(message, args, client);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
     }
 })
 
@@ -69,7 +145,7 @@ client.on("messageReactionAdd", (reaction, user) => {
         })
     }
     else if (reaction.emoji.name === "🔒") {
-        if (message.channel.name.endsWith("-ticket")) {
+        if (message.channel.name.endsWith("-ticket") || message.channel.name.endsWith("-mp")) {
             message.channel.delete()
         }
         else {
